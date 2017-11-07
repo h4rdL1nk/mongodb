cd778e31b988a4dd43411ce5e4ad890aa38314d7
h4rdL1nk <h4rdl1nk@gmail.com>
2017-11-07 15:23:40 +0100

    Added scripts dir

---
+++ scripts/user_create.js
@@ -0,0 +1,28 @@
+
+conn = new Mongo("127.0.0.1:27017");
+db = conn.getDB("admin");
+
+if ( ! db.getRole("dbs-ro")){
+
+	db.createRole(
+        	{
+                role:"dbs-ro",
+                privileges:[
+                        {resource:{db:"",collection:""},actions:["find"]}
+                ],
+                roles:[]
+        }
+);
+}
+
+if ( ! db.getUser("ro-user")){
+db.createUser(
+	{
+		user: "ro-user",
+		pwd: "123456",
+		roles: [ 
+			{role: "dbs-ro",db:"admin"} 
+		]
+	}
+);
+}
